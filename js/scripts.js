3941635c7e706cb5576491bdb4dfe3e41fc9c8aa
tyler quinn <tyler.quinn032@gmail.com>
2022-10-26 19:38:50 -0700

    add new more clear alerts

--- js/scripts.js
+++ js/scripts.js
@@ -14,14 +14,15 @@ function divide(number1, number2) {
   return number1 / number2;
 }
 
-
 const number1 = parseInt(prompt("Enter a number: "));
 const number2 = parseInt(prompt("Enter another number: "));
+const num1String = number1.toString()
+const num2String= number2.toString()
 
-window.alert("the sum of your numbers are " + add(number1, number2));
+window.alert(num1String + "+" + num2String + "=" + add(number1, number2));
 
-window.alert("the sum of your numbers are " + subtract(number1, number2));
+window.alert(num1String + "-" + num2String + "=" + subtract(number1, number2));
 
-window.alert("the sum of your numbers are " + multiply(number1, number2));
+window.alert(num1String + "*" + num2String + "=" + multiply(number1, number2));
 
-window.alert("the sum of your numbers are " + divide(number1, number2));
+window.alert(num1String + "/" + num2String + "=" + divide(number1, number2));
